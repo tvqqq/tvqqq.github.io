0441e47eb1495ded784548a1256d77859e647947
Tat Vi Quyen <tvq9612@gmail.com>
2021-03-21 21:34:58 +0700

    chore: add more info about spotify song

--- src/components/common/Spotify.js
+++ src/components/common/Spotify.js
@@ -7,8 +7,9 @@ import axios from 'axios'
 class Spotify extends Component {
     state = {
         playing: false,
-        song: 'paused...',
-        artist: ''
+        song: '',
+        artist: '',
+        url: ''
     }
 
     async componentDidMount() {
@@ -35,8 +36,13 @@ class Spotify extends Component {
                     <path stroke="#fff" d="M46 32a14 14 0 1 0-14 14 14 14 0 0 0 14-14zm-26 0a12 12 0 1 1 12 12 12 12 0 0 1-12-12z" />
                     <path stroke="#fff" d="M41 32a9 9 0 1 0-9 9 9 9 0 0 0 9-9zm-16 0a7 7 0 1 1 7 7 7 7 0 0 1-7-7z" />
                     <path d="M36 32a4 4 0 1 0-4 4 4 4 0 0 0 4-4zm-6 0a2 2 0 1 1 2 2 2 2 0 0 1-2-2zM6 34h2v2H6z" />
-                </svg>&nbsp;
-                <span>{this.state.song}</span>
+                </svg>
+                <div class="song-information">
+                    {this.state.playing ?
+                        <a href={this.state.url}>{this.state.song} - {this.state.artist}</a>
+                        : <span>paused...</span>
+                    }
+                </div>
             </p>
         )
     }
