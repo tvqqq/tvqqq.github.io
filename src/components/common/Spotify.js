17650c1854e81ed9e0c2df77a3fe3d086878f008
Tat Vi Quyen <tvq9612@gmail.com>
2021-06-19 10:19:38 +0700

    change domain sam2

--- src/components/common/Spotify.js
+++ src/components/common/Spotify.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 class Spotify extends Component {
     state = {
         playing: false,
-        song: '',
-        artist: '',
-        url: ''
+        song: ``,
+        artist: ``,
+        url: ``,
     }
 
     async componentDidMount() {
-        axios.get('https://sam.tatviquyen.name.vn/spotify/playing')
+        axios.get(`https://sam2.tatviquyen.name.vn/spotify/playing`)
             // axios.get('http://127.0.0.1:3000/spotify/playing')
-            .then(result => {
+            .then((result) => {
                 this.setState({
                     playing: result.data.playing,
                     song: result.data.song,
@@ -23,7 +23,7 @@ class Spotify extends Component {
                     url: result.data.url,
                 })
             })
-            .catch(err => {
+            .catch((err) => {
                 console.log(err)
             })
     }
@@ -31,7 +31,7 @@ class Spotify extends Component {
     render() {
         return (
             <p className="site-spotify-playing">
-                <svg id="icon-audio-disc" className={this.state.playing ? 'playing' : ''} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20" height="20">
+                <svg id="icon-audio-disc" className={this.state.playing ? `playing` : ``} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20" height="20">
                     <path stroke="#fff" d="M32 2a30 30 0 1 0 30 30A30 30 0 0 0 32 2zm0 58a28 28 0 1 1 28-28 28 28 0 0 1-28 28z" />
                     <path d="M32 6A26 26 0 0 0 6 32h2A24 24 0 0 1 32 8zM32 56v2a26 26 0 0 0 26-26h-2a24 24 0 0 1-24 24z" />
                     <path stroke="#fff" d="M32 52v2a22 22 0 0 0 22-22h-2a20 20 0 0 1-20 20z" />
@@ -39,7 +39,7 @@ class Spotify extends Component {
                     <path stroke="#fff" d="M41 32a9 9 0 1 0-9 9 9 9 0 0 0 9-9zm-16 0a7 7 0 1 1 7 7 7 7 0 0 1-7-7z" />
                     <path d="M36 32a4 4 0 1 0-4 4 4 4 0 0 0 4-4zm-6 0a2 2 0 1 1 2 2 2 2 0 0 1-2-2zM6 34h2v2H6z" />
                 </svg>
-                <div class="song-information">
+                <div className="song-information">
                     {this.state.playing ?
                         <a href={this.state.url}>{this.state.song} - {this.state.artist}</a>
                         : <span>paused...</span>
